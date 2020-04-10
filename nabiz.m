226a7a885b3c80ed52433f4142afc416c002fcd0
cgncvk <cgncvk@outlook.com>
2020-04-11 02:07:34 +0300

    initial commit

---
+++ nabiz.m
@@ -0,0 +1,21 @@
+% Bu program EKG sinyaline ait dakikadaki kalp ritmini ölçmektedir.
+
+
+%Sinyaldeki dominant tepe(peak) noktalar? say?l?r(Kalp at??lar? say?l?r)
+%Bu tepeler(peaks) iki en yak?n kom?usundan ve 1'den büyük olmak zorundad?r.
+
+nabizz = 0;
+for k = 2  : length(sig)-1
+    if(sig(k) > sig (k-1) && sig(k) > sig(k+1) && sig(k) > 1)
+        %k
+        %disp('Dominant Tepe Degeri Bulundu');
+        nabizz = nabizz + 1;
+    end
+end
+%Dakikadaki kalp at???n? bulmak için, say?lan dominant tepe noktalar?n?
+%sinyal süresine böl.
+frekans = 100;
+N = length(sig);
+saniye_cinsinden = N/frekans;
+dakika_cinsinden = saniye_cinsinden/60;
+Kalp_Atis = nabizz/dakika_cinsinden
